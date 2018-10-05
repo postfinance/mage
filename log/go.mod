ad723c8064e9e7edd380e8f49257ba37263a8062
Rene Zbinden <rene.zbinden@postfinance.ch>
2018-10-05 12:57:48 +0200

    add zap logger

---
+++ log/go.mod
@@ -0,0 +1,8 @@
+module github.com/postfinance/mage/log
+
+require (
+	github.com/magefile/mage v1.6.2
+	go.uber.org/atomic v1.3.2 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.9.1
+)
---
+++ log/log.go
@@ -0,0 +1,45 @@
+package log
+
+import (
+	"fmt"
+
+	"github.com/magefile/mage/mg"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// New creates a new zap logger.
+//
+// If MAGEFILE_DEBUG=1 the level is set to Debug, otherwise it is set to Info. If
+// MAGEFILE_VERBOSE is not set only errors are logged..
+//
+// If there is an error in logger configuration, a noop logger and
+// an error are returned.
+func New() (*zap.Logger, error) {
+	l := zap.New(nil) // noop logger
+	atom := zap.NewAtomicLevelAt(zap.InfoLevel)
+	config := zap.NewProductionConfig()
+	config.DisableStacktrace = true
+	config.DisableCaller = true
+	config.Sampling = nil
+	config.Encoding = "console"
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig.EncodeDuration = zapcore.StringDurationEncoder
+	config.Level = atom
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	atom.SetLevel(zap.ErrorLevel)
+	if mg.Debug() {
+		atom.SetLevel(zap.DebugLevel)
+		config.DisableCaller = false
+	}
+	if mg.Verbose() {
+		atom.SetLevel(zap.InfoLevel)
+	}
+	var err error
+	l, err = config.Build()
+	if err != nil {
+		return zap.New(nil), fmt.Errorf("could not create zap logger: %s", err)
+	}
+	defer l.Sync()
+	return l, nil
+}
